902ce113f9387df8b9c49dcaec558693b031bd0b
lisiver <developer.lisiver@gmail.com>
2023-02-21 14:43:24 +0900

    perf(nextjs): nextjs swc 옵션을 활성화합니다

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   reactStrictMode: false,
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
+  swcMinify: true
 }
